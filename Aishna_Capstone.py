c8b6a327110a77beb78e61478390315e61db12fe
18aishna48 <37611476+18aishna48@users.noreply.github.com>
2018-04-12 14:58:46 +0900

    Week 2

--- Aishna_Capstone.py
+++ Aishna_Capstone.py
@@ -5,43 +5,41 @@
 
 # Import SPGL
 import spgl
-
+import random
 # Create Classes
+
+
 class Player(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
 		self.frame = 0
+		self.y_acceleration = 5
+		self.y_speed = 5
+		self.strength = 7
+		self.score = 3
+		self.state = "running"
 		
-	def move(self):
-		self.fd(self.speed)
+# sprite_name.set_bounding_box(width, height)
+
+	def score(self):
+		self.score = 3
+	
+# 	def right(self):
+# 		self.setx(self.xcor() + 10)
 		
-	def increasespeed(self):
-		self.speed += 1
-		if self.speed >10:
-			self.speed = 10		
-		
-	def tick(self):
-		self.frame += 1
-		if self.frame % 10 == 0:
-			self.color("red")
-		elif self.frame % 15 == 0:
-			self.color("blue")
-			self.frame = 0
-			
-	def right(self):
-		self.setx(self.xcor() + 10)
-		
-	def jump(self):
-		self.y_acceleration += self.strength
-		self.sety(1)
+	def up(self):
+		if self.state == "running":
+			self.y_acceleration += self.strength
+			self.sety(0)
+			self.state = "jumping"
 	
 	def tick(self):
-		self.setx(self.xcor() + 3)
-		
-		if self.ycor() < 0:
-			self.y_acceleration = 0
-			self.y_speed = 0
-			self.sety(0)
+		self.setx(self.xcor())
+		if self.ycor() < -90:
+			self.y_acceleration = 1
+			self.y_speed = 1
+			self.sety(-100)
+			self.state = "running"
 			
 		self.y_acceleration += game.gravity
 		self.y_speed += self.y_acceleration
@@ -51,31 +49,59 @@ class Plant(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)	
 		self.frame = 0
+		self.speed = -6
 
-	def move(self):
+	def tick(self):
 		self.fd(self.speed)
 
+# 	def ressurect(self):
+
+# sprite_name.set_bounding_box(width, height)
 
 		
-# Create Functions
+class Bird(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)	
+		self.frame = 0
+		self.speed = -6
+	def tick(self):
+		self.fd(self.speed)
 
-# game.gravity = -2
+# Create Functions
 
 # Initial Game setup
 game = spgl.Game(800, 600, "black", "Aishna Capstone Project", 0)
+game.gravity = -1
 
 # Create Sprites
-player = Player("circle", "blue", 100, -100)
-plant = Plant("square", "white", 100, -100)
+bird = Bird("triangle", "red",random.randint(0,1000), -90)
+bird = Bird("triangle", "red",random.randint(0,1000), -90)
+player = Player("circle", "blue", -200, -100)
 
-# Create Labels
+plant = Plant("square", "white", random.randint(0,1000), -100)
 
-# Create Buttons
+
+# Create Label
+label_name = spgl.Label("MARIO RUN", "white", 50, 100)  
+label_name.set_font_name("SuperMarioBrosWii")
+label_name.set_font_size(50)
+score_label = spgl.Label("Score: 0", "white", -380, 280)
+
 
 # Set Keyboard Bindings
-game.set_keyboard_binding(spgl.KEY_RIGHT, player.right)
-game.set_keyboard_binding(spgl.KEY_LEFT, player.left)
+game.set_keyboard_binding(spgl.KEY_UP, player.up)
+# game.set_keyboard_binding(spgl.KEY_DOWN, player.down)
 
 while True:
     # Call the game tick method
-    game.tick()
+	game.tick()
+	if game.is_collision(player, plant):
+		player.score += -1
+		score_label.update("Score: {}".format(player.score))
+		
+	if game.is_collision(player, bird):
+		player.score += -1
+		score_label.update("Score: {}".format(player.score))
+		
+	if plant.xcor() < -500 or plant.xcor():
+		Plant("square", "white", random.randint(0,1000), -100)
