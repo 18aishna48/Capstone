074cefd083ba6e6262c2753f7a160d9f988dc24e
18aishna48 <37611476+18aishna48@users.noreply.github.com>
2018-04-19 15:14:19 +0900

    Week 3

--- Aishna_Capstone.py
+++ Aishna_Capstone.py
@@ -6,9 +6,15 @@
 # Import SPGL
 import spgl
 import random
+import turtle
+
 # Create Classes
 
-
+class Game(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+		self.shape("ground.gif")
+		
 class Player(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)
@@ -16,29 +22,35 @@ class Player(spgl.Sprite):
 		self.y_acceleration = 5
 		self.y_speed = 5
 		self.strength = 7
-		self.score = 3
+		self.score = 0
 		self.state = "running"
+		self.lives = 3
 		
-# sprite_name.set_bounding_box(width, height)
-
 	def score(self):
-		self.score = 3
+		self.score = 0			
 	
-# 	def right(self):
-# 		self.setx(self.xcor() + 10)
-		
+	def lives(self):
+		self.lives = 3
+	
 	def up(self):
 		if self.state == "running":
+			self.shape("mario.gif")
 			self.y_acceleration += self.strength
 			self.sety(0)
 			self.state = "jumping"
 	
+	def down(self):
+		self.set_image("Mario_Crouch.gif", 150, 145)
+		self.sety(-90)
+		self.state = "crouching"
+	
 	def tick(self):
 		self.setx(self.xcor())
-		if self.ycor() < -90:
+		
+		if self.ycor() < -80:
 			self.y_acceleration = 1
 			self.y_speed = 1
-			self.sety(-100)
+			self.sety(-90)
 			self.state = "running"
 			
 		self.y_acceleration += game.gravity
@@ -49,23 +61,24 @@ class Plant(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)	
 		self.frame = 0
-		self.speed = -6
+		self.speed = -10
+		self.shape("plant.gif")
 
 	def tick(self):
 		self.fd(self.speed)
 
-# 	def ressurect(self):
-
-# sprite_name.set_bounding_box(width, height)
-
-		
-class Bird(spgl.Sprite):
+class Shell(spgl.Sprite):
 	def __init__(self, shape, color, x, y):
 		spgl.Sprite.__init__(self, shape, color, x, y)	
 		self.frame = 0
-		self.speed = -6
+		self.speed = -10
 	def tick(self):
 		self.fd(self.speed)
+
+class Coin(spgl.Sprite):
+	def __init__(self, shape, color, x, y):
+		spgl.Sprite.__init__(self, shape, color, x, y)
+
 
 # Create Functions
 
@@ -74,11 +87,10 @@ game = spgl.Game(800, 600, "black", "Aishna Capstone Project", 0)
 game.gravity = -1
 
 # Create Sprites
-bird = Bird("triangle", "red",random.randint(0,1000), -90)
-bird = Bird("triangle", "red",random.randint(0,1000), -90)
-player = Player("circle", "blue", -200, -100)
-
-plant = Plant("square", "white", random.randint(0,1000), -100)
+player = Player("mario.gif", "blue", -200, -100)
+shell = Shell("shell.gif", "red",random.randint(100,1000), -6)
+plant = Plant("plant.gif", "white", random.randint(100,1000), -100)
+ground = Game("ground.gif", "blue", 0, -110)
 
 
 # Create Label
@@ -86,22 +98,37 @@ label_name = spgl.Label("MARIO RUN", "white", 50, 100)
 label_name.set_font_name("SuperMarioBrosWii")
 label_name.set_font_size(50)
 score_label = spgl.Label("Score: 0", "white", -380, 280)
+lives_label = spgl.Label("Lives: 3", "white", -280, 280)
 
 
 # Set Keyboard Bindings
 game.set_keyboard_binding(spgl.KEY_UP, player.up)
-# game.set_keyboard_binding(spgl.KEY_DOWN, player.down)
+game.set_keyboard_binding(spgl.KEY_DOWN, player.down)
 
 while True:
     # Call the game tick method
 	game.tick()
 	if game.is_collision(player, plant):
-		player.score += -1
-		score_label.update("Score: {}".format(player.score))
+		player.lives += -1
+		lives_label.update("Lives: {}".format(player.lives))
+		plant.goto(random.randint(300,600), -100)
 		
-	if game.is_collision(player, bird):
-		player.score += -1
-		score_label.update("Score: {}".format(player.score))
 		
-	if plant.xcor() < -500 or plant.xcor():
-		Plant("square", "white", random.randint(0,1000), -100)
+	if game.is_collision(player, shell):
+		player.lives += -1
+		lives_label.update("Lives: {}".format(player.lives))
+		shell.goto(random.randint(600,900), -20)
+
+	if player.xcor() == plant.xcor() and player.xcor() == shell.xcor():
+		player.score += 1
+		
+	if plant.xcor() < -500:
+		plant.goto(random.randint(300,600), -100)
+		
+	if shell.xcor() < -500:
+		shell.goto(random.randint(600,900), -20)
+
+# 	if player.lives > -1:
+# 		label_name = spgl.Label("GAME OVER", "white", -200, 100)  
+# 		label_name.set_font_name("SuperMarioBrosWii")
+# 		label_name.set_font_size(50)		
